0baa48e900c895f37981605c3500af71f98f111c
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2023-11-19 18:26:35 -0800

    Fix lexer tokenization issues

--- src/lib/lex.cpp
+++ src/lib/lex.cpp
@@ -1,3 +1,4 @@
+
 #include "lex.h"
 
 std::deque<Token> Lexer::tokenize(std::string input) {
@@ -8,13 +9,30 @@ std::deque<Token> Lexer::tokenize(std::string input) {
     int col = 0;
     bool partOfVar = false;
 
-    for (int i = 0; (unsigned)i < input.length(); i++) {
+    for (size_t i = 0; (unsigned)i < input.length(); i++) {
         col++;
         temp += input[i];
-        //Number tokens
-        if (isalpha(input[i]) || (input[i] == '_') || (partOfVar && isdigit(input[i]))) {
+        //std::cout << "temp: " << temp << " line: " << token.getLine() << " col " << token.getCol() << std::endl;
+
+        //bools
+        if (temp == "true" || temp == "false" || temp == "if" || temp == "while" || temp == "print" || temp == "else") {
+            token = Token(line, col - temp.length() + 1, temp);
+            tokens.push_back(token);
+            temp = "";
+
+            // Skip over the characters that were part of the "true" or "false" token
+            i += temp.length();
+
+            continue;
+        }
+        else if(input[i] == '{' || input[i] == '}'){
+            token = Token(line, col, temp);
+            tokens.push_back(token);
+            temp = "";
+        }
+        else if (isalpha(input[i]) || (input[i] == '_') || (partOfVar && isdigit(input[i]))) {
             partOfVar = true;
-            if (!isalpha(input[i+1]) && !isdigit(input[i+1]) && input[i+1] != '_') {
+            if (i + 1 < input.length() && !isalpha(input[i+1]) && !isdigit(input[i+1]) && input[i+1] != '_') {
                 token = Token(line, col - temp.length() + 1, temp);
                 tokens.push_back(token);
                 temp = "";
@@ -22,13 +40,61 @@ std::deque<Token> Lexer::tokenize(std::string input) {
             }
         }
         else if (input[i] == '=') {
+            if (i + 1 < input.length() && input[i-1] != '=' && input[i+1] != '=' && input[i-1] != '!' &&  // = operator
+                input[i+1] != '!' && input[i-1] != '<' && input[i-1] != '>') {
+                token = Token(line, col, temp);
+                tokens.push_back(token);
+                temp = "";
+            }
+            else if (input[i-1] == '=') {      // == operator
+                if(tokens.back().getToken() != "<=" && tokens.back().getToken() != ">="  && // if previous token uses = prevent it from incorrectly printing == instead of =
+                tokens.back().getToken() != "!=" && tokens.back().getToken() != "=="){
+                    token = Token(line, col - 1, "==");
+                    tokens.push_back(token);
+                    temp = "";
+                }
+                else if(input[i+1] != '='){ //prevents print of = when next node would make it ==
+                   token = Token(line, col, temp);
+                    tokens.push_back(token);
+                    temp = ""; 
+                }
+            }
+            else if (input[i-1] == '!') {      // != operator
+                token = Token(line, col - 1, "!=");
+                tokens.push_back(token);
+                temp = "";
+            }
+            else if (input[i-1] == '<') {      // <= operator
+                token = Token(line, col - 1, "<=");
+                tokens.pop_back();
+                tokens.push_back(token);
+                temp = "";
+            }
+            else if (input[i-1] == '>') {      // >= operator
+                token = Token(line, col - 1, ">=");
+                tokens.pop_back();            //THIS AND PREVIOUS POP_BACK COULD MESS WITH CODE LATER
+                tokens.push_back(token);
+                temp = "";
+            }
+        }
+        else if ((input[i] == '<' || input[i] == '>') && i + 1 < input.length()) {
+            token = Token(line, col, temp);
+            tokens.push_back(token);
+            temp = "";
+        }
+        else if (input[i] == '^') {            // TODO: Add token for XOR in token.cpp
+            token = Token(line, col, temp);
+            tokens.push_back(token);
+            temp = "";
+        }
+        else if (input[i] == '|') {            // TODO: Add token for OR in token.cpp
             token = Token(line, col, temp);
             tokens.push_back(token);
             temp = "";
         }
         else if (isdigit(input[i]) || input[i] == '.') {
             // push full number
-            if (!isdigit(input[i+1]) && input[i+1] != '.') {
+            if (i + 1 < input.length() && !isdigit(input[i+1]) && input[i+1] != '.') {
                 token = Token(line, col - temp.length() + 1, temp);
                 tokens.push_back(token);
                 temp = "";
@@ -39,7 +105,12 @@ std::deque<Token> Lexer::tokenize(std::string input) {
             tokens.push_back(token);
             temp = "";
         }
-        else if ((input[i] == '+') || (input[i] == '-') || (input[i] == '*') || (input[i] == '/')) {
+        else if ((input[i] == '+') || (input[i] == '-') || (input[i] == '*') || (input[i] == '/') || input[i] == '%') { // Add modulo and token for modulo
+            token = Token(line, col, temp);
+            tokens.push_back(token);
+            temp = "";
+        }
+        else if (input[i] == '&') { // TODO: Add token for AND in token.cpp
             token = Token(line, col, temp);
             tokens.push_back(token);
             temp = "";
@@ -49,14 +120,14 @@ std::deque<Token> Lexer::tokenize(std::string input) {
                 line++;
                 col = 0;
             }
-            else if (temp != " " && temp != "\t") {
+            else if (temp != " " && temp != "\t" && temp != "!") {
                 token = Token(line, col, temp);
                 tokens.push_back(token);
             }
             temp = "";
         }
         //Error handling
-        if (token.getType() == ERROR) {
+        if (token.getType() == "ERROR") {
             std::cout << "Syntax error on line " << token.getLine() << " column " << token.getCol() << ".\n";
             throw(1);
         }
@@ -71,5 +142,19 @@ std::deque<Token> Lexer::tokenize(std::string input) {
             tokens.push_back(token);
         }
     }
+
+    //check if the lex overall is valid (has float, identifier, or bool)
+    bool validValue = false;
+    for(size_t i = 0; i < tokens.size(); i++){
+        if(tokens[i].getType() == "float" || tokens[i].getType() == "identifier"){ //ADD TRUE FALSE TO THIS
+            validValue = true;
+        }
+    }
+    if(!validValue){
+        std::cout << "Syntax error on line " << token.getLine() << " column " << token.getCol() << ".\n";
+        throw(1);
+    }
+
+
     return tokens;
 }
