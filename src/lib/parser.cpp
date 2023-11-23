7d245435ff5456093fc0553ca021843ed1f9d41c
MeronDT <merondebt@gmail.com>
2023-11-22 21:53:58 -0800

    Update parser.cpp

--- src/lib/parser.cpp
+++ src/lib/parser.cpp
@@ -43,45 +43,39 @@ Node* Parser::parseSmallWrapper(std::deque<Token>& tokens) {
     return tree.release();
 }
 
-Node* Parser::parseOperand(std::deque<Token>& tokens) {
+std::unique_ptr<Node> Parser::parseOperand(std::deque<Token>& tokens) {
     if (this->nextToken.getToken() == "") {
         std::cout << "Parse error a" << std::endl; // Add more details later
         throw(2);
     }
+
     if (this->nextToken.getType() == booleanVal) {
-        std::unique_ptr<BoolNode> boolean(new BoolNode(this->nextToken.getToken()));
-        eatToken(tokens);
-        return boolean.release();
+        return std::make_unique<BoolNode>(this->nextToken.getToken());
     }
     else if (this->nextToken.getType() == identifier) {
-        std::unique_ptr<IdentifierNode> nodeLeaf(new IdentifierNode(nextToken.getToken()));
-        eatToken(tokens);
-        return nodeLeaf.release();
+        return std::make_unique<IdentifierNode>(nextToken.getToken());
     }
     else if (this->nextToken.getType() == number) {
-        std::unique_ptr<LeafNode> leafNode(new LeafNode(std::stold(nextToken.getToken())));
-        eatToken(tokens);
-        return leafNode.release();
+        return std::make_unique<LeafNode>(std::stold(nextToken.getToken()));
     }
     else if (this->nextToken.getToken() == "(") {
         eatToken(tokens);
-        std::unique_ptr<Node> variableNode(parseAssign(tokens));
+        std::unique_ptr<Node> variableNode = parseAssign(tokens);
 
         if (nextToken.getToken() == ")") {
             eatToken(tokens);
-            return variableNode.release();
+            return variableNode;
         } else {
             std::cout << "Parse Error: Missing closing parenthesis" << std::endl;
             throw(2);
         }
     }
-    else{
+    else {
         std::cout << "Unexpected token at line " << nextToken.getLine() << " column " << nextToken.getCol() << ": " << nextToken.getToken() << std::endl;
         throw(2);
     }
-
-    return nullptr;
 }
+
 
 
 Node* Parser::parseAssign(std::deque<Token>& tokens) {
